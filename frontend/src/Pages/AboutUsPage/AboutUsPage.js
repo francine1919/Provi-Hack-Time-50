8e61f48ef52071fa4ca8af81a3d49e22c21b5971
Dayane <you@example.com>
2022-05-01 18:07:16 -0300

    atualizando

--- frontend/src/Pages/AboutUsPage/AboutUsPage.js
+++ frontend/src/Pages/AboutUsPage/AboutUsPage.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
+import Logoabout from "../../Assets/images/logoabout.png"
+
 export default function AboutUsPage() {
   return (
     <div>
+      <ContainerHeader>
       <Header/>
       <div>AboutUsPage</div>
+      </ContainerHeader>
+
+      <img src={logoabout} alt=""/>
+      <h1>Somos um grupo de 7 entusiastas emprol da preservação ambiental. </h1>
+      <p>Através do Hackathon da Provi, fomos desafiados a influenciar e, quem sabe até mudar 
+        o cenário ambiental atual através da tecnologia.</p>
+      <p>Assim nasceu a Pegada verde, afim de aproximar pessoas que podem ajudar a mudar o cenário ambiental 
+        e ainda ajudar na renda de produtores rurais e proprietários de veículos elétricos.</p>
+      
+      <h2>Nós te ajudamos a reduzir a sua pegada de carbono!</h2>
+      
+      <h1>A equipe</h1>
+      <div>
+
+        
+      </div>
+   
     </div>
+
   );
 }
